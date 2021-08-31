4e2a71a68eda52f4cb61b7f520ad67ae32b4905c
james-skelton <james.skelton@newcastle.ac.uk>
2021-08-31 14:48:30 +0100

    Added test to ensure the node specifications are importable as expected

--- tests/test_nedrexdb.py
+++ tests/test_nedrexdb.py
@@ -3,3 +3,12 @@ from nedrexdb import __version__
 
 def test_version() -> None:
     assert __version__ == "0.1.0"
+
+
+def test_node_imports() -> None:
+    from nedrexdb.db.models.nodes.disorder import Disorder
+    from nedrexdb.db.models.nodes.drug import Drug
+    from nedrexdb.db.models.nodes.gene import Gene
+    from nedrexdb.db.models.nodes.pathway import Pathway
+    from nedrexdb.db.models.nodes.protein import Protein
+    from nedrexdb.db.models.nodes.signature import Signature
