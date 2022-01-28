3ce438ba5869a0ecf7d49c93277d06a57bc18b9c
James Skelton <james.skelton@newcastle.ac.uk>
2022-01-28 16:30:09 +0000

    Added file containing the Tissue node type.

---
+++ nedrexdb/db/models/nodes/tissue.py
@@ -0,0 +1,39 @@
+import datetime as _datetime
+
+from pydantic import BaseModel as _BaseModel, Field as _Field, StrictStr as _StrictStr
+from pymongo import UpdateOne as _UpdateOne
+
+from nedrex.db import models
+
+
+class TissueBase(models.MongoMixin):
+    node_type: str = "Tissue"
+    collection_name: str = "tissue"
+
+    @classmethod
+    def set_indexes(cls, db):
+        db[cls.collection_name].create_index("primaryDomainId", unique=True)
+
+
+class Tissue(_BaseModel, TissueBase):
+    primaryDomainId: _StrictStr = ""
+    domainIds: list[str] = _Field(default_factory=list)
+    displayName: _StrictStr = ""
+    organ: _StrictStr = ""
+
+    def generate_update(self):
+        tnow = _datetime.datetime.utcnow()
+
+        query = {"primaryDomainId": self.primaryDomainId}
+        update = {
+            "$addToSet": {"domainIds": {"$each": self.domainIds}},
+            "$set": {
+                "type": self.node_type,
+                "updated": tnow,
+                "displayName": self.displayName,
+                "organ": self.organ,
+            },
+            "$setOnInsert": {"created": tnow},
+        }
+
+        return _UpdateOne(query, update, upsert=True)
