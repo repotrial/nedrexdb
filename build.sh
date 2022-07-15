5e2040b415dc22722a6647f0cdf35bc251433d0b
James Skelton <james.skelton@newcastle.ac.uk>
2022-07-15 15:41:20 +0000

    Added build script. Note that this has hard coded filenames in it.

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+./build.py update --conf .licensed_config.toml
+./set_metadata.py --config .licensed_config.toml --version live
+
+./build.py update --conf .open_config.toml
+./set_metadata.py --config .open_config.toml --version live
